b8bc4379d1032369cf84bf260ffbaa3f1b9df0a6
Halyson <halyson97@gmail.com>
2021-03-04 23:23:05 -0300

    Add react-router-native and partial Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,23 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
+
+import {NativeRouter, Route} from 'react-router-native';
+
+import Home from './partials/Home';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>Hello world :)</Text>
-          </View>
+        <ScrollView>
+          <NativeRouter>
+            <Route exact path="/" component={Home} />
+          </NativeRouter>
         </ScrollView>
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-});
 
 export default App;
